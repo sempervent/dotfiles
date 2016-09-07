03bebc2c54a2b57fd467a1003fade2f3b1175734
sempervent <jngrant9@gmail.com>
2016-09-07 16:02:12 -0400

    Added color support

--- install_dots.sh
+++ install_dots.sh
@@ -8,25 +8,25 @@
 dir=~/dotfiles
 olddir=~/dotfiles_old
 files="vimrc bashrc tmux.conf gitconfig"
-
+source colors.sh
 ############ work
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+echo -e "${NC} Creating ${LCYAN}${olddir}${NC} for backup of any existing dotfiles in ~"
+mkdir -p ${olddir}
+echo -e "...${GREEN}done\n"
 
 # change to dotfiles dir
-echo "Changing directory to $dir directory"
-cd $dir
-echo "...done"
+echo -e "${NC}Changing directory to ${LCYAN}${dir}${NC} directory"
+cd ${dir}
+echo -e "...${GREEN}done${NC}\n"
 
 # move any existing dotfiles in homedir to dotfiles_old directory
 # then create symlinks
 for file in $files; do
-   echo "Moving any existing dotfiles from ~ to $olddir"
+   echo -e "Moving any existing ${LRED}${file}${NC} from ~ to $olddir"
    mv ~/.$file ~/dotfiles_old/
-   echo "Creating symlink to $file in home directory."
+   echo -e "\tCreating symlink to ${GREEN}${file}${NC} in home directory.\n"
    ln -s $dir/$file ~/.$file
 done
 
