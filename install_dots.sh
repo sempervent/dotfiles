1408f88f4888bb30204ecd229042bfcde19ca623
sempervent <jngrant9@gmail.com>
2016-10-06 09:25:54 -0400

    Added tmux powerline config

--- install_dots.sh
+++ install_dots.sh
@@ -6,13 +6,16 @@
 
 ############ variables
 dir=~/dotfiles
-olddir=~/dotfiles_old
+olddir=~/.dotfiles_old
 i3dir=~/.i3
+configdir=~/.config
 files="vimrc bashrc tmux.conf gitconfig xinitrc Xresources Xdefaults"
 i3files='i3config'
+pwrlnconfig=~/.config/powerline
+pwrln="tmux_powerline.conf"
 source $dir/colors.sh
-############ work
 
+############ work
 # create dotfiles_old in homedir
 echo -e "${NC}Creating ${LCYAN}${olddir}${NC} for backup of any existing dotfiles in ~"
 mkdir -p ${olddir}
@@ -33,9 +36,34 @@ for file in $files; do
 done
 
 # move i3 files
-echo -e "Changing to ${YELLOW}${i3dir}${NC}"
-cd ${i3dir}
-echo -e "Backing up i3config"
-mv ~/.i3/config ~/.i3/config.back
-echo -e "\tCreating symlink to ${GREEN}${i3files}${NC} in ${CYAN}${i3dir}${NC}.\n"
-ln -s ~/dotfiles/i3config ~/.i3/config 
+echo -e "Checking for ${CYAN}i3${NC} directory"
+if [ -d "$i3dir" ];then
+   echo -e "Changing to ${YELLOW}${i3dir}${NC}"
+   cd ${i3dir}
+   echo -e "Backing up i3config"
+   mv ~/.i3/config ~/.i3/config.back
+   echo -e "\tCreating symlink to ${GREEN}${i3files}${NC} in ${CYAN}${i3dir}${NC}.\n"
+   ln -s ~/dotfiles/i3config ~/.i3/config 
+else
+   echo -e "\t${RED}${i3dir}${NC} not found.\n"
+fi
+
+cd $dir
+
+# powerline config files
+echo -e "Checking for ${CYAN}${pwrlnconfig}${NC} directory."
+if [ -d "$pwrlnconfig" ];then
+   echo -e "Found dir ${YELLOW}${pwrlnconfig}${NC}."
+   echo -e "\tBacking up Powerline specific bindings"
+   for conf in $pwrln; do
+      echo -e "\tFound ${LRED}${conf}${NC} in ${CYAN}${pwrlnconfig}${NC}."
+      mv $pwrlnconfig/${conf} ${olddir}/${conf}
+      echo -e "\tCreating symlink to ${GREEN}${conf}${NC} in ${pwrlnconfig}."
+      ln -s ${dir}/${conf} "${pwrlnconfig}/${conf}"
+   done
+else
+   echo -e "\t\t\t\t${RED}${pwrlnconfig}{$NC} not found."
+fi
+
+# report exit status
+echo -e "\nYour ${GREEN}Dotfiles${NC} have been updated.\n"
