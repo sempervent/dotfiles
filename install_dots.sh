324244e7a373c88ec26087aa8980386b014a276f
sempervent <jngrant9@gmail.com>
2017-12-28 17:01:49 -0500

    whiptail dialog for install_dots.sh

--- install_dots.sh
+++ install_dots.sh
@@ -8,12 +8,44 @@
 dir=~/dotfiles
 olddir=~/.dotfiles_old
 i3dir=~/.i3
-configdir=~/.config
+# configdir=~/.config
 files="vimrc bashrc tmux.conf gitconfig xinitrc Xresources Xdefaults"
 i3files='i3config'
 pwrlnconfig=~/.config/powerline
 pwrln="tmux_powerline.conf"
-source $dir/colors.sh
+# shellcheck source="$HOME/dotfiles/colors.sh"
+source "$dir/colors.sh"
+brand="Sempervent's dotfiles"
+
+apts="i3wm tmux i3 i3blocks conky"
+pips="powerline-status"
+
+############ getopts
+while getopts ":i" opt; do
+   case $opt in
+      i)
+         if hash whiptail 2>/dev/null; then
+            if [ "$(id -nu)" != "root" ]; then
+               sudo -k
+               pass=$(whiptail --backtitle "$brand Installer" --title "Authentication required" --passwordbox "Installing $brand requires software to properly use, and you passed the -i flag, so that's what this is going to do.\n\n[sudo] Password for user $USER:" 12 50 3>&2 2>&1 1>&3- )
+               exec sudo apt-get install apt i3wm tmux i3 i3blocks conky
+            fi
+         else
+            sudo apt-get install whiptail
+         fi
+         echo -e "-i was triggered. Install has finished?" >&2
+         ;;
+      \?)
+         echo -e "Invalid option: -$OPTARG" >&2
+   esac
+done
+
+
+if [ $EUID != 0 ]; then
+   sudo "$0" "$@"
+   exit $?
+fi
+
 
 ############ work
 # create dotfiles_old in homedir
@@ -23,23 +55,23 @@ echo -e "\t\t\t\t\t...${GREEN}done\n"
 
 # change to dotfiles dir
 echo -e "${NC}Changing directory to ${LCYAN}${dir}${NC} directory"
-cd ${dir}
+cd ${dir} || exit
 echo -e "\t\t\t\t\t...${GREEN}done${NC}\n"
 
 # move any existing dotfiles in homedir to dotfiles_old directory
 # then create symlinks
 for file in $files; do
    echo -e "Moving any existing ${LRED}${file}${NC} from ~ to $olddir"
-   mv ~/.$file ${olddir} 
+   mv "$HOME/.$file" ${olddir} 
    echo -e "\tCreating symlink to ${GREEN}${file}${NC} in home directory.\n"
-   ln -s $dir/$file ~/.$file
+   ln -s "$dir/$file" "$HOME/.$file"
 done
 
 # move i3 files
 echo -e "Checking for ${CYAN}i3${NC} directory"
 if [ -d "$i3dir" ];then
    echo -e "Changing to ${YELLOW}${i3dir}${NC}"
-   cd ${i3dir}
+   cd ${i3dir} || exit
    echo -e "Backing up i3config"
    mv ~/.i3/config ~/.i3/config.back
    echo -e "\tCreating symlink to ${GREEN}${i3files}${NC} in ${CYAN}${i3dir}${NC}.\n"
@@ -48,7 +80,7 @@ else
    echo -e "\t${RED}${i3dir}${NC} not found.\n"
 fi
 
-cd $dir
+cd $dir || exit
 
 # powerline config files
 echo -e "Checking for ${CYAN}${pwrlnconfig}${NC} directory."
